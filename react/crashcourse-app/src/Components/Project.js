47931e5cc7a11f3e7c8039968a6348b5e4a5db33
SATOSHI TOKUNO <RAKUMASA@HOTMAIL.COM>
2017-08-18 08:14:07 -0700

    First update

---
+++ react/crashcourse-app/src/Components/Project.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Projects extends Component {
+  render() {
+    return (
+      <div className="App">
+        My Projects
+      </div>
+    );
+  }
+}
+
+export default Projects;
